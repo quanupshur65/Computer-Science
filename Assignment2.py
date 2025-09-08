b32a53199e82248c063a0293bdb39d08c6097e37
LaQuan Upshur <laquan.upshur001@albright.edu>
2025-09-08 09:09:20 -0400

    Add files via upload

--- Assignment2.py
+++ Assignment2.py
@@ -1,8 +1,35 @@
-# 2-1
-message="Hello Python World"
+message= "hey Quan, would you like to learn python today."
 print(message)
-#2-2
-message="Hello Python Crash Course World"
+first_name="Quan"
+last_name="Upshur"
+full_name=f"{first_name} {last_name}"
+print(full_name)
+name="Quan"
+print(name.lower())
+print(name.upper())
+print(name.title())
+famous_person = "Albert Einstein"
+message = f'{famous_person} once said, "A person who never made a mistake never tried anything new."'
 print(message)
-message= "Hello Python Crash Course Reader"
+name_with_whitespace = "\t\n Quan Upshur \n\t"
+print("Original with whitespace:")
+print(name_with_whitespace)
+
+print("\nUsing lstrip():")
+print(name_with_whitespace.rstrip())
+
+print("\nUsing rstrip():")
+print(name_with_whitespace.rstrip())
+
+print("\nUsing strip():") 
+print(name_with_whitespace.strip())
+filename = "python_notes.txt"
+print(filename.removesuffix(".txt")) 
+print(5 + 3)   #addition
+print(10 - 2)  #subtraction
+print(4 * 2)
+print(16 / 2)
+favorite_number = 2
+message = f"My favorite number is {favorite_number}."
 print(message)
+import this
