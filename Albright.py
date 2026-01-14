a93e0e7a9b8318ee825c9d8fe193c9a850105adc
LaQuan Upshur <laquan.upshur001@albright.edu>
2026-01-14 12:30:04 -0500

    Add files via upload

---
+++ Albright.py
@@ -0,0 +1,15 @@
+Group: [Darwin, Quan, Kendall]
+
+Our project will be an Albright College Sports Team manager site. The program will 
+allow users to view and manage albright sports team basic informational needs, 
+including player names, positions, stats, injury reports,and yearly progress. 
+Users will be able to add, remove, and edit players, view upcoming games, and show individual, 
+and team summaries.This project connects to the Albright theme by focusing on campus athletics
+while letting us practice real world programming like data storage, and user interaction.
+
+https://github.com/quanupshur65/Computer-Science.git
+
+-Python
+-github
+-Github Copilot
+-Pygame
