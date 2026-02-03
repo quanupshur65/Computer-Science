47ec49b6d0ed01b502dead555ba85562d247c234
LaQuan Upshur <laquan.upshur001@albright.edu>
2026-02-03 13:51:49 -0500

    Add files via upload

---
+++ BlackJackfixed.py
@@ -0,0 +1,73 @@
+import random
+def draw_card(hand, deck):
+   hand.append(deck.pop())  
+def calculate_score(hand):
+    score = 0
+    aces = 0
+    
+    for card in hand:
+        if card in ["J", "Q", "K"]:
+            score += 10
+        elif card == "A": 
+            score += 11
+            aces += 1
+        else:
+            score += int(card)
+
+    while score > 21 and aces > 0:
+        score -=10
+        aces -=1
+    return score
+    
+def print_status(player_hand, dealer_hand, show_dealer_card=True):
+    print("/nPlayer hand:", player_hand)
+    print("Player score:", calculate_score(player_hand))
+
+    if show_dealer_card:
+        print("Dealer hand:", dealer_hand)
+        print("Dealer score:", calculate_score(dealer_hand))
+    else:
+        print("dealer shows:", dealer_hand[0])
+        
+def main():
+    deck = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    player_hand = []
+    dealer_hand = []
+    random.shuffle(deck)
+
+    draw_card(dealer_hand, deck)
+    draw_card(player_hand, deck)
+    draw_card(player_hand, deck)
+
+    while True:
+        print_status(player_hand, dealer_hand, False)
+
+        if calculate_score(player_hand) > 21:
+            print("You busted! Dealer wins.")
+            return
+        
+        choice = input("Hit or stay? (h/s): ").lower()
+
+        if choice == "h":
+            draw_card(player_hand, deck)
+        elif choice == "s":
+            break
+        else:
+            print("Please enter 'h' or 's' .")
+
+    while calculate_score(dealer_hand) < 17:
+        draw_card(dealer_hand, deck)
+    print_status(player_hand, dealer_hand, True)
+    player_score = calculate_score(player_hand)
+    dealer_score = calculate_score(dealer_hand)
+
+    if dealer_score > 21:
+        print("Dealer busted! You win!")
+    elif dealer_score > player_score:
+        print("Dealer wins.")
+    elif dealer_score < player_score:
+        print("You win!")
+    else:
+        print("It's a tie!")
+if __name__ == "__main__":
+    main()
